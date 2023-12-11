fdbb850767622f29a6d401c7b260ca0a78d1bbf2
guptasarthak03 <guptasarthak03@gmail.com>
2023-12-11 19:52:32 +0530

    image data

---
+++ src/assets/imageData.js
@@ -0,0 +1,52 @@
+export const imageData = [
+  {
+    option1: './assets/images/1-correct.png',
+    option2: './assets/images/1-fake.png',
+    correct: './assets/images/1-fake.png',
+  },
+  {
+    option1: './assets/images/2-correct.png',
+    option2: './assets/images/2-fake.png',
+    correct: './assets/images/2-fake.png',
+  },
+  {
+    option1: './assets/images/3-correct.png',
+    option2: './assets/images/3-fake.png',
+    correct: './assets/images/3-fake.png',
+  },
+  {
+    option1: './assets/images/4-correct.png',
+    option2: './assets/images/4-fake.png',
+    correct: './assets/images/4-fake.png',
+  },
+  {
+    option1: './assets/images/5-correct.png',
+    option2: './assets/images/5-fake.png',
+    correct: './assets/images/5-fake.png',
+  },
+  {
+    option1: './assets/images/6-correct.png',
+    option2: './assets/images/6-fake.png',
+    correct: './assets/images/6-fake.png',
+  },
+  {
+    option1: './assets/images/7-correct.png',
+    option2: './assets/images/7-fake.png',
+    correct: './assets/images/7-fake.png',
+  },
+  {
+    option1: './assets/images/8-correct.png',
+    option2: './assets/images/8-fake.png',
+    correct: './assets/images/8-fake.png',
+  },
+  {
+    option1: './assets/images/9-correct.png',
+    option2: './assets/images/9-fake.png',
+    correct: './assets/images/9-fake.png',
+  },
+  {
+    option1: './assets/images/10-correct.png',
+    option2: './assets/images/10-fake.png',
+    correct: './assets/images/10-fake.png',
+  },
+];
