c3c4bb58538b585ab9b502561187268c4e6fc61c
guptasarthak03 <guptasarthak03@gmail.com>
2023-12-11 17:58:00 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Quiz from './components/Quiz/Quiz';
+
+const App = () => {
+  return (
+    <div className='app-container'>
+      <Quiz />
+    </div>
+  );
+};
+
+export default App;
